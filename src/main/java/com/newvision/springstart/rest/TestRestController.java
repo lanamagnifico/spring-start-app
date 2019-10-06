393e1bf43624b4cf88a676fae4fd2bdbd5c4d9ef
lanamagnifico <svetlana.magnifico@gmail.com>
2019-10-06 19:12:58 +0200

    Add JPA with h2 datasource

--- src/main/java/com/newvision/springstart/rest/TestRestController.java
+++ src/main/java/com/newvision/springstart/rest/TestRestController.java
@@ -1,4 +1,4 @@
-package com.newvision.springstart.controller;
+package com.newvision.springstart.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
